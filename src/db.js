166dd3f0638939ff4026590a7e2e204aa553bf5d
dima0073231 <147907949+dima0073231@users.noreply.github.com>
2025-12-08 19:49:42 +0200

    add

--- src/db.js
+++ src/db.js
@@ -1,22 +1,16 @@
-// src/db.js
-require('dotenv').config();
-const mongoose = require('mongoose');
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGO_URI);
 
 async function connectDB() {
-  const uri = process.env.DATABASE_URL || process.env.MONGO_URI;
-  if (!uri) {
-    console.error('❌ No DATABASE_URL / MONGO_URI in .env');
-    throw new Error('No DB URI');
-  }
   try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('✅ Connected to MongoDB Atlas');
+    await client.connect();
+    console.log("MongoDB connected");
+
+    return client.db(); // вернёт БД
   } catch (err) {
-    console.error('❌ MongoDB connection error:', err.message || err);
-    throw err;
+    console.error("MongoDB connection failed:", err);
+    process.exit(1);
   }
 }
 
